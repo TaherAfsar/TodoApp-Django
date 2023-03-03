a9a9a1b3fe9626de6e6400a99d45fb8fb75b7969
Taher <taherafsar41@gmail.com>
2023-03-03 21:02:58 +0530

    fixing errors in deployment

--- todolist/settings.py
+++ todolist/settings.py
@@ -135,4 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ]
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '../static')
